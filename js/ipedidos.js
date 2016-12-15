ea247160e65fc0be8929a06a5e3b4fc3a6317974
juliotoscano <juliotoscanodebrito@gmail.com>
2016-12-15 09:24:40 -0300

    icon and others

--- js/ipedidos.js
+++ js/ipedidos.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
 
+    //Barra de escolha das opções do breadcrumb
+    $('.selected').parent().find('p').css('color', 'white').css('font-weight','bold');
+
     $('.menu-option').bind('click', function(event) {
-    $('#breadcrumb').find('.selected').removeClass('selected');
-    $(this).find('div').addClass('selected');
+        $('.selected').parent().find('p').css('color', '#EEEEEE').css('font-weight','normal');
+        $('#breadcrumb').find('.selected').removeClass('selected');
+        $(this).find('div').addClass('selected');
+        $('.selected').parent().find('p').css('color', 'white').css('font-weight','bold');
+        var i = $('.selected').attr('id');
+        if (option_id == 'option_2') {
+            $('.article').load("registros_pedido.html");
+        }else{}
     });
+
+    //Carregar formularios.
+    var option_menu = $('.selected');
+    var option_id = $(option_menu).attr('id');
+    if (option_id == 'option_1') {
+        $('.article').load("formulario_pedido_1.html");
+    }else{};
 
 });
